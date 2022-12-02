595cbd853cfb7472b834a51475237314f2fef67f
hkcwby <alexanderstevenson@me.com>
2022-12-02 14:58:44 -0600

    adding on the left and right pannel components that form the main app

--- src/components/structureComponents/LeftPanel.js
+++ src/components/structureComponents/LeftPanel.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function LeftPanel(props){
     return (<>
-        <div id = "left-page" className = "split">
+        <div id="left-page" className="split">
             <h2>Memos</h2>  
             
         </div>        
--- src/components/structureComponents/RightPanel.js
+++ src/components/structureComponents/RightPanel.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function RightPanel(props){
 
     return (<>
-        <div id= "right-page" className = "">
+        <div id="right-page" className="split">
             <h2>Memo Detail</h2>
 
         </div>
