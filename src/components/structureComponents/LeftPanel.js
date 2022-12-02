be79bb433118127d6a209d607d37aaf07ead4377
hkcwby <alexanderstevenson@me.com>
2022-12-02 14:57:45 -0600

    removing unwanted css from create react app

--- src/components/structureComponents/LeftPanel.js
+++ src/components/structureComponents/LeftPanel.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function LeftPanel(props){
     return (<>
-        <div id = "left-page" className = "">
+        <div id = "left-page" className = "split">
             <h2>Memos</h2>  
             
         </div>        
