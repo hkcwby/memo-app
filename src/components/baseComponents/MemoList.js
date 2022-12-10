968a197ed9c0552a07e3788c9098f63e6518f422
hkcwby <alexanderstevenson@me.com>
2022-12-10 01:46:40 -0600

    fixed issues around memo deletion and associated hover css

--- src/components/baseComponents/MemoList.js
+++ src/components/baseComponents/MemoList.js
@@ -4,8 +4,8 @@ import Button from "./Button"
 export default function MemoList(props){
     return <ul>
         {props.data.map(item=>
-            <li key={item.id} onClick={()=>props.onMemoClick(item.id)}>
-                {item.title}
+            <li key={item.id} >
+                <span className="memo-entry" onClick={()=>props.onMemoClick(item.id)}>{item.title}</span>    
                 <Button className="btn-outline btn-delete" onClick={() => props.onDeleteClick(item.id)}>X</Button>
                 </li>)}
     </ul>;
