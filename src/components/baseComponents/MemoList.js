77eb7e996b3e7aea6eebd33b2273ce9abac5f9b0
hkcwby <alexanderstevenson@me.com>
2022-12-13 02:10:07 -0600

    further css mods to allow for smaller screens

--- src/components/baseComponents/MemoList.js
+++ src/components/baseComponents/MemoList.js
@@ -4,7 +4,7 @@ import Button from "./Button"
 export default function MemoList(props){
     return <ul>
         {props.data.map(item=>
-            <li key={item.id} >
+            <li className="left" key={item.id} >
                 <span className="memo-entry" onClick={()=>props.onMemoClick(item.id)}>{item.title}</span>    
                 <Button className="btn-outline btn-delete" onClick={() => props.onDeleteClick(item.id)}>X</Button>
                 </li>)}
